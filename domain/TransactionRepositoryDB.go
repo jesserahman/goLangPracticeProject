52795640794bc15257e5a85595a805f4b43f7010
jesserahman <jesserahman@yahoo.com>
2021-11-17 17:10:05 -0500

    got bank account deposit and withdrawl working

--- domain/TransactionRepositoryDB.go
+++ domain/TransactionRepositoryDB.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jmoiron/sqlx"
 	"log"
 	"strconv"
+	"strings"
 )
 
 type TransactionRepositoryDb struct {
@@ -14,8 +15,25 @@ type TransactionRepositoryDb struct {
 }
 
 func (t TransactionRepositoryDb)ExecuteTransaction(transaction Transaction) (*Transaction, *errs.AppError){
+	// get current bank account details
+	accountQuery := fmt.Sprintf("select * from banking.accounts where account_id = %s", transaction.AccountId)
+	account := Account{AccountId: transaction.AccountId}
+	err := t.dbClient.QueryRowx(accountQuery).StructScan(&account)
+	if err != nil {
+		logger.Error("Error querying accounts table " + err.Error())
+		return nil, errs.NewUnexpectedError("unexpected database error")
+	}
+
 	// update bank account
-	accountUpdate := fmt.Sprintf("Update banking.accounts Set amount = %f WHERE account_id = '%s'", transaction.Amount, transaction.AccountId)
+	var newBalance float64
+	if strings.ToLower(transaction.TransactionType) == "withdraw" {
+		newBalance = account.Amount - transaction.Amount
+	} else if strings.ToLower(transaction.TransactionType) == "deposit" {
+		newBalance = account.Amount + transaction.Amount
+	} else {
+		return nil, errs.NewUnexpectedError("invalid transaction type")
+	}
+	accountUpdate := fmt.Sprintf("Update banking.accounts Set amount = %f WHERE account_id = '%s'", newBalance, transaction.AccountId)
 	result, err := t.dbClient.Exec(accountUpdate, )
 	log.Println("result: ", result)
 	if err != nil {
